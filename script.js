9719966ddddf633b93058eb63dae68da935622a1
philpatt <phillip.s.patterson1@gmail.com>
2018-03-21 12:58:31 -0700

    working on reverseWord problem

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Code Wars problem
 
+// create a function that determines the factorial of any number
 function factorialize(num){
     for(var answer=1; num>0; num--){
         answer*=num;
@@ -8,3 +9,12 @@ function factorialize(num){
 }
 console.log(factorialize(5));
 
+// create a function that reverses the WORDS in a string
+
+function reverseWord(str){
+    var words = str.split(' ');
+    for(var i=words.length-1; i=0; i--){
+        console.log(i);
+    }
+}
+console.log(reverseWord('Well hello there'));
