1907b0da7d6680fadb13f4783b8d0c1111feba44
philpatt <phillip.s.patterson1@gmail.com>
2018-03-21 16:43:38 -0700

    adding more probs

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ function reverseWord(str){
     return reversedArr.join(' ');
 }
 console.log(reverseWord('well hello there'));
+
+// create a function that determines if a word is a palindrome
+
+function isPalindrome(str){
+    var reversedArr = [];
+    var words = str.split('');
+    for(var i = words.length -1; i>=0 ;i-- ){
+        // console.log(reversedArr);        
+        reversedArr.push(words[i]);
+    }
+
+    if(str == reversedArr.join('')){
+        return 'palindrome';
+    } else {
+        return 'not';
+    }
+}
+console.log(isPalindrome('poop'));
+
+
+
+// Given an array of 1000 integers, write a function that returns the 10 largest numbers. unsorted
+
+function largest10(numArr){
+
+
+}
+
+largest10([1234,123,2,5,7,8,9,10,33,13,12,65]);
+
+// 5 cards, compare who has poker hands 
+
+function compare (hand1, hand2){
+    
+}
