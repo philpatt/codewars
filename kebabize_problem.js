22acbf887612677cbfeb00372faeca120c672e80
philpatt <phillip.s.patterson1@gmail.com>
2018-11-07 19:45:14 -0800

    reworking kebabize problemn

--- kebabize_problem.js
+++ kebabize_problem.js
@@ -10,10 +10,18 @@
 function kebabize(kebab){
     kebab = kebab.split('')
     for(l in kebab ){
-        if (kebab[l] === kebab[l].toUpperCase()){
+        console.log('**',kebab[l])
+        if (kebab[0] === kebab[0].toUpperCase()){
+            console.log('here 1', kebab[l]);
+            kebab[l] = kebab[l].toLowerCase();
+        } else if (parseInt(kebab[l])) {
+            console.log('here 2', kebab[l]);
+            kebab.splice(l, 1);
+        } else if (kebab[l] === kebab[l].toUpperCase()) {
+            console.log('here 3', kebab[l]);
             kebab[l] = '-' + kebab[l].toLowerCase();
         }
     } 
     return kebab.join('')
 }
-console.log(kebabize('camelsHaveHumps'))
+console.log(kebabize('myCamelHas3Humps'))
